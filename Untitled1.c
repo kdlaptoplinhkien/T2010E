000a89b0e598c4a9ba581649aaf2a02dc08cfc38
kdlaptoplinhkien <kdlaptoplinhkien@gmail.com>
2020-10-06 18:39:11 +0700

    Create Untitled1.c

---
+++ Untitled1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	//chuong trinh nam o day
+int n;
+printf("Nhap gia tri:");
+scanf("%d", &n)/n/n;
+
+if(n>=0){
+	//nhung lenh se lam khi yes
+printf("n la so duong");
+}
+	else {
+		//nhung lenh se lam khi no
+		printf("n la so am");
+	}
+}
